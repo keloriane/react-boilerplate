426f1b09711715277d0fecae1205529e28773b53
Keloriane <kevin.flbt@gmail.com>
2018-11-08 10:21:07 +0100

    initial-commit-react-boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.scss";
+import { createHashHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
+ReactDOM.render(
+  <div id="app">
+    <h1>lets have fun!!!</h1>
+  </div>,
+  document.getElementById("root")
+);
